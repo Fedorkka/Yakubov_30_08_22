82bc9234d0d14344cb7a2a6ee8312c206f070800
Fedor <fuzhegov@mail.ru>
2022-10-29 16:54:53 +0300

    v1.1.1

--- main.cpp
+++ main.cpp
@@ -9,10 +9,13 @@ using namespace std;
 int main() {
     int bounds[2]= {20, 20};
     int num1, num2;
-    sf::Color colors[]= {sf::Color::Blue, sf::Color::Red,
-                         sf::Color::Magenta, sf::Color::Green,
-                         sf::Color::Yellow, sf::Color::Cyan
-                        };
+    sf::Color colors[]= {sf::Color(230, 25, 75), sf::Color(60, 180, 75), sf::Color(255, 225, 25),
+                         sf::Color(0, 130, 200), sf::Color(245, 130, 48), sf::Color(145, 30, 180),
+                         sf::Color(70, 240, 240), sf::Color(240, 50, 230), sf::Color(210, 245, 60),
+                         sf::Color(250, 190, 212), sf::Color(0, 128, 128), sf::Color(220, 190, 255),
+                         sf::Color(170, 110, 40), sf::Color(255, 250, 200), sf::Color(128, 0, 0),
+                         sf::Color(170, 255, 195), sf::Color(128, 128, 0), sf::Color(255, 215, 180),
+                         sf::Color(0, 0, 128)};
     sf::VertexArray line(sf::LinesStrip, 2);
     Field f(600, 600, 2);
     cout<<"Enter the number of Generators: ";
@@ -63,15 +66,15 @@ int main() {
 
         for(int i=0; i<f.get_stations_count(); i++) {
             station_body.setPosition(f.get_station_pos_x(i)+bounds[0], f.get_stations_pos_y(i)+bounds[0]);
-            station_body.setFillColor(colors[i%6]);
+            station_body.setFillColor(colors[i%19]);
             for(int j=0; j<f.get_claster_size(i); j++) {
-                gen_body.setFillColor(colors[i%6]);
+                gen_body.setFillColor(colors[i%19]);
                 gen_body.setPosition(f.get_claster(i, j).X()+bounds[0], f.get_claster(i, j).Y()+bounds[1]);
                 gen_body.setRadius(5*f.get_claster(i, j).LVL());
                 line[0].position = sf::Vector2f(f.get_claster(i, j).X()+gen_body.getRadius()+bounds[0], f.get_claster(i, j).Y()+gen_body.getRadius()+bounds[1]);
-                line[0].color  = colors[i%6];
+                line[0].color  = colors[i%19];
                 line[1].position = sf::Vector2f(f.get_station_pos_x(i)+bounds[0]+10, f.get_stations_pos_y(i)+bounds[1]+10);
-                line[1].color = colors[i%6];
+                line[1].color = colors[i%19];
                 window.draw(line);
                 window.draw(gen_body);
             }
